428302d5924d74e6001b74d2dd7471e503e91779
PINTO0309 <rmsdh122@yahoo.co.jp>
2018-07-09 01:13:53 +0900

    New

--- MobileNetSSDwithRealSenseTF.py
+++ MobileNetSSDwithRealSenseTF.py
@@ -3,10 +3,14 @@ if sys.version_info.major < 3 or sys.version_info.minor < 4:
     print("Please using python3.4 or greater!")
     exit(1)
 
+import os
 import numpy as np
 import argparse
 import cv2
 import pyrealsense2 as rs
+import tensorflow as tf
+from utils import label_map_util
+from utils import visualization_utils as vis_util
 
 sys.path.append('..')
 inWidth = 300
@@ -15,26 +19,13 @@ WHRatio = inWidth / float(inHeight)
 inScaleFactor = 0.007843
 meanVal = 127.5
 
-# Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-
-# Start streaming
-pipeline.start(config)
-
 try:
-#    net = cv2.dnn.readNetFromTensorflow('/home/b920405/git/MobileNet-SSD-RealSense-TF/frozen_inference_graph.pb',
-#                                        '/home/b920405/git/MobileNet-SSD-RealSense-TF/graph.pbtxt')
-
-    MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
     CWD_PATH = os.getcwd()
-    PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
-    PATH_TO_LABELS = os.path.join(CWD_PATH,'data','mscoco_label_map.pbtxt')
-
-    net = cv2.dnn.readNetFromTensorflow('ssdlite_mobilenet_v2_coco.pb')
+    MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
+    PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, 'frozen_inference_graph.pb')
+    PATH_TO_LABELS = os.path.join(CWD_PATH, 'data', 'mscoco_label_map.pbtxt')
     swapRB = True
+    NUM_CLASSES = 90
     classNames = { 0: 'background',
         1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus',
         7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant',
@@ -53,6 +44,47 @@ try:
         80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock',
         86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush' }
 
+    ## Load the label map.
+    # Label maps map indices to category names, so that when the convolution
+    # network predicts `5`, we know that this corresponds to `airplane`.
+    # Here we use internal utility functions, but anything that returns a
+    # dictionary mapping integers to appropriate string labels would be fine
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+
+    # Load the Tensorflow model into memory.
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+        sess = tf.Session(graph=detection_graph)
+
+    # Input tensor is the image
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    # Output tensors are the detection boxes, scores, and classes
+    # Each box represents a part of the image where a particular object was detected
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    # Each score represents level of confidence for each of the objects.
+    # The score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    # Number of objects detected
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    # Configure depth and color streams RealSense D435
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+    # Start streaming
+    pipeline.start(config)
+
     while True:
 
         # Wait for a coherent pair of frames: depth and color
@@ -66,14 +98,33 @@ try:
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
 
-        blob = cv2.dnn.blobFromImage(color_image, inScaleFactor, (inWidth, inHeight), (meanVal, meanVal, meanVal), swapRB)
-        net.setInput(blob)
-        detections = net.forward()
+        frame_expanded = np.expand_dims(color_image, axis=0)
 
-        height = color_image.shape[0]
-        width = color_image.shape[1]
+        # Perform the actual detection by running the model with the image as input
+        (boxes, scores, classes, num) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={image_tensor: frame_expanded})
 
-        num_valid_boxes = int(detections.shape[2])
+        # Draw the results of the detection (aka 'visulaize the results')
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            color_image,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=1,
+            min_score_thresh=0.85)
+
+        #https://github.com/tensorflow/models/blob/master/research/object_detection/utils/visualization_utils.py
+
+        #meters = depth_frame.as_depth_frame().get_distance(x1+int((x2-x1)/2), y1+int((y2-y1)/2))
+        #label_text = LABELS[int(class_id)] + " (" + str(percentage) + "%)"+ " {:.2f}".format(meters) + " meters away"
+        #cv2.putText(color_image, label_text, (label_left, label_bottom), cv2.FONT_HERSHEY_SIMPLEX, 0.5, label_text_color, 1)
+
+
+
+
 
         if num_valid_boxes > 0:
 
