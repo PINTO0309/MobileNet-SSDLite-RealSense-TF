ff1879caef4169169c289bab6a821738632d02cf
PINTO0309 <rmsdh122@yahoo.co.jp>
2018-07-09 20:08:38 +0900

    New

--- MobileNetSSDwithRealSenseTF.py
+++ MobileNetSSDwithRealSenseTF.py
@@ -22,29 +22,30 @@ meanVal = 127.5
 pipeline = None
 
 try:
-    CWD_PATH = os.getcwd()
+    HOME_PATH = os.path.expanduser('~')
+    CWD_PATH = 'tensorflow/models/research/object_detection'
     MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
-    PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, 'frozen_inference_graph.pb')
-    PATH_TO_LABELS = os.path.join(CWD_PATH, 'data', 'mscoco_label_map.pbtxt')
+    PATH_TO_CKPT = os.path.join(HOME_PATH, CWD_PATH, MODEL_NAME, 'frozen_inference_graph.pb')
+    PATH_TO_LABELS = os.path.join(HOME_PATH, CWD_PATH, 'data', 'mscoco_label_map.pbtxt')
     swapRB = True
     NUM_CLASSES = 90
-    classNames = { 0: 'background',
-        1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus',
-        7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant',
-        13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat',
-        18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear',
-        24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag',
-        32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard',
-        37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove',
-        41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle',
-        46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon',
-        51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange',
-        56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut',
-        61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed',
-        67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse',
-        75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven',
-        80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock',
-        86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush' }
+#    classNames = { 0: 'background',
+#        1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus',
+#        7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant',
+#        13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat',
+#        18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear',
+#        24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag',
+#        32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard',
+#        37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove',
+#        41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle',
+#        46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon',
+#        51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange',
+#        56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut',
+#        61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed',
+#        67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse',
+#        75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven',
+#        80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock',
+#        86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush' }
 
     ## Load the label map.
     # Label maps map indices to category names, so that when the convolution
@@ -99,6 +100,8 @@ try:
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
+        height = color_image.shape[0]
+        width = color_image.shape[1]
 
         frame_expanded = np.expand_dims(color_image, axis=0)
 
