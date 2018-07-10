018ccf25a18f10a3600c14098a053491219b71fc
PINTO0309 <rmsdh122@yahoo.co.jp>
2018-07-10 23:56:46 +0900

    New

--- MobileNetSSDwithRealSenseTF.py
+++ MobileNetSSDwithRealSenseTF.py
@@ -13,6 +13,9 @@ sys.path.append('..')
 from utils import label_map_util
 import visualization_utils as vis_util
 
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
 
 inWidth = 300
 inHeight = 300
@@ -70,54 +73,102 @@ try:
 
     # Start streaming
     pipeline.start(config)
-
-    while True:
-
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            continue
-
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-        height = color_image.shape[0]
-        width = color_image.shape[1]
-
-        frame_expanded = np.expand_dims(color_image, axis=0)
-
-        # Perform the actual detection by running the model with the image as input
-        (boxes, scores, classes, num) = sess.run(
-            [detection_boxes, detection_scores, detection_classes, num_detections],
-            feed_dict={image_tensor: frame_expanded})
-
-        # Draw the results of the detection (aka 'visulaize the results')
-        vis_util.visualize_boxes_and_labels_on_image_array(
-            color_image,
-            np.squeeze(boxes),
-            np.squeeze(classes).astype(np.int32),
-            np.squeeze(scores),
-            category_index,
-            use_normalized_coordinates=True,
-            line_thickness=2,
-            min_score_thresh=0.55,
-            depth_frame=depth_frame,
-            height=height,
-            width=width)
-
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', color_image)
-        if cv2.waitKey(1)&0xFF == ord('q'):
-            break
-
 except:
     import traceback
     traceback.print_exc()
+    # Stop streaming
+    if pipeline != None:
+        pipeline.stop()
+    print("\n\nFinished\n\n")
+    sys.exit()
 
+
+def init():
+    glClearColor(0.7, 0.7, 0.7, 0.7)
+
+def idle():
+    glutPostRedisplay()
+
+def resizeview(w, h):
+    glViewport(0, 0, w, h)
+    glLoadIdentity()
+    glOrtho(-w / 640, w / 640, -h / 480, h / 480, -1.0, 1.0)
+
+def keyboard(key, x, y):
+    key = key.decode('utf-8')
+    if key == 'q':
+        sys.exit()
+
+def camThread():
+    # Wait for a coherent pair of frames: depth and color
+    frames = pipeline.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+    color_frame = frames.get_color_frame()
+    if not depth_frame or not color_frame:
+        return
+    
+    # Convert images to numpy arrays
+    depth_image = np.asanyarray(depth_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
+    height = color_image.shape[0]
+    width = color_image.shape[1]
+
+    frame_expanded = np.expand_dims(color_image, axis=0)
+
+    # Perform the actual detection by running the model with the image as input
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: frame_expanded})
+
+    # Draw the results of the detection (aka 'visulaize the results')
+    img = vis_util.visualize_boxes_and_labels_on_image_array(
+        color_image,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=2,
+        min_score_thresh=0.55,
+        depth_frame=depth_frame,
+        height=height,
+        width=width)
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glColor3f(1.0, 1.0, 1.0)
+    glEnable(GL_TEXTURE_2D)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glBegin(GL_QUADS) 
+    glTexCoord2d(0.0, 1.0)
+    glVertex3d(-1.0, -1.0,  0.0)
+    glTexCoord2d(1.0, 1.0)
+    glVertex3d( 1.0, -1.0,  0.0)
+    glTexCoord2d(1.0, 0.0)
+    glVertex3d( 1.0,  1.0,  0.0)
+    glTexCoord2d(0.0, 0.0)
+    glVertex3d(-1.0,  1.0,  0.0)
+    glEnd()
+    glFlush()
+    glutSwapBuffers()
+
+try:
+    glutInitWindowPosition(0, 0)
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE )
+    glutCreateWindow("DEMO")
+    glutFullScreen()
+    glutDisplayFunc(camThread)
+    glutReshapeFunc(resizeview)
+    glutKeyboardFunc(keyboard)
+    init()
+    glutIdleFunc(idle)
+    glutMainLoop()
+except:
+    import traceback
+    traceback.print_exc()
 finally:
-
     # Stop streaming
     if pipeline != None:
         pipeline.stop()
